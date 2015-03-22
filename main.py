7869784458ba1db1a7c9d2eb3d02f1335b74d426
Julia <julia.koveshnikova@gmail.com>
2015-03-22 21:22:55 +0300

    Version 1.1
    * code refactoring (functions for getting params added, global result
    dict added)
    * checking if parameter in cloud is empty and putting 'n\a'
    * parsing publisher from web added
    * empty or incorrect cmd-line argument exception added
    * errorlog beautified
    * messagebox type changed
    * loglevel variable added
    * code comments beautified

--- main.py
+++ main.py
@@ -2,156 +2,205 @@ from bs4 import BeautifulSoup			#Additional component for HTML parsing
 import urllib
 import subprocess
 import os
+import sys
 import re
-import tkMessageBox
 import codecs
+import ctypes
 
-#Getting file list from cmd-line arguments:
-for arg in sys.argv:
-	folder = arg
-fileslst =  os.listdir(folder) 
+# loglevel = 1 - additional logs to cmd-line for debugging
+loglevel = 0 
+
+# Global Dictionaries 							
+keysLocalMap = {
+		'Original Name:': 'name',
+		'Publisher:': 'publisher',
+		'Product:': 'product',
+		'Description:': 'descr',
+		'MD5:': 'md5',
+		'SHA1:': 'sha1',
+		'SHA256:': 'sha256',
+		'Prod version:': 'version',
+		'Copyright:': 'copyright'
+	}
+keysWebMap = {
+		'File name:': 'name',
+		'Publisher:': 'publisher',
+		'Product:': 'product',
+		'Description:': 'descr',
+		'MD5:': 'md5',
+		'SHA-1:': 'sha1',
+		'SHA-256:': 'sha256',
+		'File size:': 'size',
+		'Product version:': 'version',
+		'Copyright:': 'copyright'
+	}
 
 def findLocalKey(keyName):
 	return keysLocalMap.get(keyName, None)
 
 def findWebKey(keyName):
-    return keysWebMap.get(keyName, None)
+	return keysWebMap.get(keyName, None)
 
-#Dictionaries for file propreties:
-parsedFileWebInfo = {
-    'name': '',
-    'publisher': '',
-    'product': '',
-    'descr': '',
-    'md5': '',
-    'sha1': '',
-    'sha256': '',
-    'size': '',
-    'version': '',
-    'copyright': ''
-}
+# Processing local file information
+def getLocalResult(filename):
+	parsedLocalFileInfo = {
+		'name': '',
+		'publisher': '',
+		'product': '',
+		'descr': '',
+		'md5': '',
+		'sha1': '',
+		'sha256': '',
+		'size': '',
+		'version': '',
+		'copyright': ''
+	}
+	# getting file propreties to temporary text file
+	subprocess.call("sigcheck.exe -a -h "+ folder +'/'+ filename +" >> localfiledesc.txt", shell=True)
+	lineEl = []
+	# opening text file in latin-1 encoding (to solve unicode problems)
+	input_attribute_file = codecs.open("localfiledesc.txt", encoding='latin-1')
+	# itterations for every line in text file split data separated by TABs
+	for line in input_attribute_file:
+		lineEl = line.split('\t')
+		if (len(lineEl) > 1):				# excluding lines with additional information
+			keyEl = lineEl[1]				
+			valueEl = lineEl[2][0:-2]		# removing line ending
+			if (keyEl):
+				keyName = findLocalKey(keyEl)	
+				if keyName is not None:
+					# latin-1 used to solve unicode problem
+					parsedLocalFileInfo[keyName] = valueEl.encode('latin-1').strip()
+	input_attribute_file.close()
+	# remove temporary text file
+	os.remove("localfiledesc.txt")
+	# get filesize and put it to the dictionary
+	statinfo = os.stat(folder +'/'+ filename)
+	sizeEl = unicode(statinfo.st_size)
+	parsedLocalFileInfo['size'] = sizeEl
+	# the file name is given to uppercase (to avoid mismatches due to register)
+	parsedLocalFileInfo['name'] = filename	
+	if loglevel == 1:
+		print (parsedLocalFileInfo)
+	return parsedLocalFileInfo
 
-parsedLocalFileInfo = {
-    'name': '',
-    'publisher': '',
-    'product': '',
-    'descr': '',
-    'md5': '',
-    'sha1': '',
-    'sha256': '',
-    'size': '',
-    'version': '',
-    'copyright': ''
-}
+# Processing cloud file information
+def getWebResult(filename, sha):
+	parsedFileWebInfo = {
+		'name': '',
+		'publisher': '',
+		'product': '',
+		'descr': '',
+		'md5': '',
+		'sha1': '',
+		'sha256': '',
+		'size': '',
+		'version': '',
+		'copyright': ''
+	}
+	# checking opened url coincedence with original to avoid homepage redirect in case of no data in cloud
+	url = urllib.urlopen("http://www.herdprotect.com/" + filename + "-" + str(sha) + ".aspx").geturl()
+	if url == ("http://www.herdprotect.com/" + filename + "-" + str(sha) + ".aspx"):							
+		herdprotectHtml = urllib.urlopen("http://www.herdprotect.com/" + filename + "-" + str(sha) + ".aspx")
+		global filescnt
+		filescnt = filescnt+1
+		# parsing html
+		soup = BeautifulSoup(herdprotectHtml)
+		for param in soup.find_all("div", class_="keyvaluepair"):
+			keyEl = param.find(class_="key")
+			valueEl = param.find(class_="value")
 
-keysWebMap = {
-    'File name:': 'name',
-    'Publisher:': 'publisher',
-    'Product:': 'product',
-    'Description:': 'descr',
-    'MD5:': 'md5',
-    'SHA-1:': 'sha1',
-    'SHA-256:': 'sha256',
-    'File size:': 'size',
-    'Product version:': 'version',
-    'Copyright:': 'copyright'
-}
+			if (keyEl):
+				parsedKeyName = keyEl.string 
+				keyName = findWebKey(parsedKeyName)
+				if keyName is not None:
+					parsedFileWebInfo[keyName] = valueEl.get_text(strip=True).encode('latin-1').strip()
+		# parsing size from web to kb
+		webSize = parsedFileWebInfo['size']
+		if (webSize):
+			pattern = '\(([0-9\,]+)[^\)]+\)'
+			convertedSize = re.search(pattern, webSize).groups()[0].replace(",", "")
+			parsedFileWebInfo['size'] = unicode(convertedSize)
+		#parsing publisher
+		webPublisher = parsedFileWebInfo['publisher']
+		if (webPublisher):
+			pattern = '([\w\s\.]*)'
+			convertedPublisher = re.search(pattern, webPublisher).groups()[0]
+			parsedFileWebInfo['publisher'] = unicode(convertedPublisher)
+		# getting checksum values to upper case (to avoid mismatches due to register)
+		parsedFileWebInfo['md5'] = parsedFileWebInfo['md5'].upper()
+		parsedFileWebInfo['sha1'] = parsedFileWebInfo['sha1'].upper()
+		parsedFileWebInfo['sha256'] = parsedFileWebInfo['sha256'].upper()
+		# If there is no value for parameter in cloud put standart "n/a"
+		for keyValue in parsedFileWebInfo:
+			if parsedFileWebInfo[keyValue] == "":
+				parsedFileWebInfo[keyValue] = "n/a"
+		if loglevel == 1:
+			print (parsedFileWebInfo)
+	# if url is not the same as we opened - there is no data in cloud.
+	else:
+		errorlog.write ('\nNo data in Cloud.')
+		global cloudExistanceFlag
+		cloudExistanceFlag = 'false'
+	global webUrl
+	webUrl = url
+	return parsedFileWebInfo
 
-keysLocalMap = {
-    'Original Name:': 'name',
-    'Publisher:': 'publisher',
-    'Product:': 'product',
-    'Description:': 'descr',
-    'MD5:': 'md5',
-    'SHA1:': 'sha1',
-    'SHA256:': 'sha256',
-    'Prod version:': 'version',
-    'Copyright:': 'copyright'
-}
+
+#Getting file list from cmd-line arguments:
+for arg in sys.argv:
+	folder = arg
+try:
+	files = os.listdir(folder)
+except OSError:	
+	ctypes.windll.user32.MessageBoxA(0, "Please specify existing folder or path with the files as an argument.\nExample: > main.py testfolder", "Checker Report", 0)
+	sys.exit()
+fileslst =  os.listdir(folder)
 
 # Counters for final report:
 globalerrorcnt = 0
+filescntall = 0
 filescnt = 0
+localResult = {}
+webResult = {}
 
 # Error log started:
 errorlog = open('log.txt', 'w')
-errorlog.write("HerdProtect Checker report:")
+errorlog.write("HerdProtect Checker report:\n-----------------------------")
 
 # Starting itterations for file list:
 for item in fileslst:
-	errorcnt = 0 										# error for one file counter
-	flag = 'true'										# flag signaling the presence file data in the cloud
-	if item[-1] == '\n':								# cutting new line ending
+	errorcnt = 0 
+	filescntall = filescntall + 1
+	cloudExistanceFlag = 'true'				# flag signaling the presence file data in the cloud
+	if item[-1] == '\n':		# cutting new line ending
 		filename = item[0:-1]
 	else:
 		filename = item
 	print
-	print('File in progress: ' + filename)				# progress output to the cmd-line
-	errorlog.write ('\nFile Name: ' + filename)			# progress output to the error log
-	subprocess.call("sigcheck.exe -a -h "+ folder +'/'+ filename +" >> localfiledesc.txt", shell=True)		# getting file propreties to temporary text file
-	lineEl = []
-	input_attribute_file = codecs.open("localfiledesc.txt", encoding='latin-1')								# opening text file in latin-1 encoding (to solve unicode problems)
-	for line in input_attribute_file:					# itterations for every line in text file
-		lineEl = line.split('\t')						# data in text file separated by tabs
-		if (len(lineEl) > 1):							# excluding lines with additional information
-			keyEl = lineEl[1]							# getting Name of file proprety value
-			valueEl = lineEl[2][0:-2]					# removing line ending
-			if (keyEl):									# checking value existance
-				keyName = findLocalKey(keyEl)			# getting name of parameter from dictionary
-				if keyName is not None:					# do we need this parameter? check in dictionary
-					parsedLocalFileInfo[keyName] = valueEl
-	input_attribute_file.close()
-	os.remove("localfiledesc.txt")						# remove temporary text file
+	print('File in progress: ' + filename)
+	errorlog.write ('\n\nFile Name: ' + filename)
+	
+	localResult[filename] = getLocalResult(filename)
+	sha = localResult[filename]['sha1']
+	webResult[filename] = getWebResult(filename, sha)
 
-	statinfo = os.stat(folder +'/'+ filename)			# get file size
-	sizeEl = unicode(statinfo.st_size)					# convert file size from longint to string
-	parsedLocalFileInfo['size'] = sizeEl				# put file size value to the dict
-	parsedLocalFileInfo['name'] = parsedLocalFileInfo['name'].upper()		# the file name is given to uppercase (to avoid mismatches due to register)
-	#print parsedLocalFileInfo												# output resulting dictionary to cmd for debugging (commented by default)
-
-	url = urllib.urlopen("http://www.herdprotect.com/" + filename + "-" + parsedLocalFileInfo['sha1'] + ".aspx").geturl()		# checking url opened
-	if url == ("http://www.herdprotect.com/" + filename + "-" + parsedLocalFileInfo['sha1'] + ".aspx"):							# checking opened url coincedence with original to avoid homepage redirect in case of no data in cloud
-		herdprotectHtml = urllib.urlopen("http://www.herdprotect.com/" + filename + "-" + parsedLocalFileInfo['sha1'] + ".aspx")# opening url
-		filescnt = filescnt+1 												# file counter increased
-		soup = BeautifulSoup(herdprotectHtml)								# preparing html for parsing
-		for param in soup.find_all("div", class_="keyvaluepair"):			# zooming to parameters div
-		    keyEl = param.find(class_="key")								# getting Name of file proprety value
-		    valueEl = param.find(class_="value")							# getting parameter value
-
-		    if (keyEl):														# cheking value existance
-		        parsedKeyName = keyEl.string 								# converting parameter to string
-		        keyName = findWebKey(parsedKeyName)							# getting name of parameter from dictionary
-
-		        if keyName is not None:										# do we need this parameter? check in dictionary
-		            parsedFileWebInfo[keyName] = valueEl.string
+	# if flag is not 'false' proceed file properties comparison
+	if cloudExistanceFlag != 'false':
+		localRes = localResult[filename]
+		webRes = webResult[filename]
+		for keyName in webRes:
+			if webRes[keyName] != localRes[keyName]:
+				errorlog.write ("\n   Parameter Name: " + str(keyName))
+				errorlog.write ("\n      Cloud: " + str(webRes[keyName]))
+				errorlog.write ("\n      Local: " + str(localRes[keyName]))
+				errorcnt = errorcnt+1
+		globalerrorcnt = globalerrorcnt + errorcnt
+		errorlog.write ("\nURL: " + str(webUrl))
+		errorlog.write ('\nErrors found in file: ' + filename + ' : '+ str(errorcnt))
 		
-		webSize = parsedFileWebInfo['size']									#getting file size value from dict
-		if (webSize):														# value exists?
-			pattern = '\(([0-9\,]+)[^\)]+\)'								# preparing regexp
-			convertedSize = re.search(pattern, webSize).groups()[0].replace(",", "")	# finding size in bytes from string
-			parsedFileWebInfo['size'] = unicode(convertedSize)				# convert file size to string
-
-		parsedFileWebInfo['md5'] = parsedFileWebInfo['md5'].upper()			# getting checksum values to upper case (to avoid mismatches due to register)
-		parsedFileWebInfo['name'] = parsedFileWebInfo['name'].upper()		# the file name is given to uppercase (to avoid mismatches due to register)
-		parsedFileWebInfo['sha1'] = parsedFileWebInfo['sha1'].upper()
-		parsedFileWebInfo['sha256'] = parsedFileWebInfo['sha256'].upper()
-		#print parsedFileWebInfo											# output resulting dictionary to cmd for debugging (commented by default)
-	else:																	# if url is not the same as we opened - there is no data in cloud.
-		errorlog.write ('\nNo data in Cloud.')
-		flag = 'false'														# flag signaling the presence file data in the cloud
-
-	if flag != 'false':														# if flag is not 'false' proceed file properties comparison
-		for keyName in parsedFileWebInfo:									# for all propreties in dictionare proceed
-			if parsedLocalFileInfo[keyName] != parsedFileWebInfo[keyName]:	# are they equal?
-				errorlog.write ("\n   Parameter Name: " + str(keyName))		# parameter mismatch
-				errorlog.write ("\n      Cloud: " + parsedFileWebInfo[keyName].encode('utf-8','ignore'))		# mismatched value in cloud
-				errorlog.write ("\n      Local: " + parsedLocalFileInfo[keyName].encode('utf-8','ignore'))		# mismatched value local
-				errorcnt = errorcnt+1 										# local error counter increased
-		globalerrorcnt = globalerrorcnt + errorcnt							# global error counter increased
-		errorlog.write ('\nErrors found in file: ' + filename + ' : '+ str(errorcnt))	# file report to error log
-		errorlog.write
-errorlog.write ('\nErrors found global: ' + str(globalerrorcnt))			# global report to error log
+errorlog.write ('\n\nErrors found global: ' + str(globalerrorcnt) + '\n-----------------------------')
 errorlog.close()
 
 # Show final messagebox to user with counters and inforamtion
-tkMessageBox.showinfo(title="Checker Report", message="Files Checked: " + str(filescnt) + "\nErrors Found: " + str(globalerrorcnt) + "\nSee log.txt for errors")
+ctypes.windll.user32.MessageBoxA(0, "Files in folder: " + str(filescntall) + "\nFiles checked: " + str(filescnt) + "\nErrors Found: " + str(globalerrorcnt) + "\nSee log.txt for errors", "Checker Report", 0)
